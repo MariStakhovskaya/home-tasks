c1c2581aa3eae0f7697e65e796e23645a539c35e
MariStakhovskaya <maryia.jegorova@gmail.com>
2021-01-21 09:55:39 +0300

    Updates

---
+++ precache-manifest.d87b198b754547b76713c11cf64ec869.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad56a29cc9424c630ada794adead1316",
+    "url": "/home-tasks/index.html"
+  },
+  {
+    "revision": "e2085174071710df9d4d",
+    "url": "/home-tasks/static/css/main.8f0c0822.chunk.css"
+  },
+  {
+    "revision": "a5eec3b31ca7866aef3a",
+    "url": "/home-tasks/static/js/2.490fbe74.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/home-tasks/static/js/2.490fbe74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2085174071710df9d4d",
+    "url": "/home-tasks/static/js/main.759b3524.chunk.js"
+  },
+  {
+    "revision": "08e17a2123cdaee402e1",
+    "url": "/home-tasks/static/js/runtime-main.e2248177.js"
+  }
+]);
