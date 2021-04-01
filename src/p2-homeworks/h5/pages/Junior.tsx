73b9bf3f6b1f5fcf7b41990db31eeab7a0f778a5
MariStakhovskaya <maryia.jegorova@gmail.com>
2021-04-01 15:51:42 +0300

    add 9 HW

--- src/p2-homeworks/h5/pages/Junior.tsx
+++ src/p2-homeworks/h5/pages/Junior.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import HW7 from "../../h7/HW7";
 import HW8 from "../../h8/HW8";
+import HW9 from "../../h9/HW9";
 
 
 
@@ -9,8 +10,8 @@ function Junior() {
         <div>
             <HW7/>
             <HW8/>
-            {/*<HW9/>
-            <HW10/>
+            <HW9/>
+           {/* <HW10/>
             <HW11/>
             <HW12/>*/}
 
--- src/p2-homeworks/h9/Clock.tsx
+++ src/p2-homeworks/h9/Clock.tsx
@@ -1,49 +1,55 @@
 import React, {useState} from "react";
 import SuperButton from "../h4/common/c2-SuperButton/SuperButton";
+import s from "./HW9.module.css"
 
 function Clock() {
     const [timerId, setTimerId] = useState<number>(0);
-    const [date, setDate] = useState<Date>();
+    const [date, setDate] = useState<Date>(new Date());
     const [show, setShow] = useState<boolean>(false);
 
     const stop = () => {
+        clearTimeout(timerId)
         // stop
     }
     const start = () => {
         stop();
         const id: number = window.setInterval(() => {
-            // setDate
+            setDate(new Date())
         }, 1000);
         setTimerId(id);
     }
 
     const onMouseEnter = () => {
-        // show
+        setShow(true)
     };
     const onMouseLeave = () => {
+        setShow(false)
         // close
     };
 
-    const stringTime = "Time"; // fix with date
-    const stringDate = "Date"; // fix with date
+    let month = date.toLocaleString('ru', {month: "long"});
+
+    const stringTime = date.getHours() + ' : ' + date.getMinutes() + ' : ' + date.getSeconds(); // fix with date
+    const stringDate = date.getDate() + ' ' + (month) + ' ' + date.getFullYear()  // fix with date
 
     return (
-        <div>
-            <div
-                onMouseEnter={onMouseEnter}
-                onMouseLeave={onMouseLeave}
+        <div className={s.container}>
+            <div className={s.time}
+                 onMouseEnter={onMouseEnter}
+                 onMouseLeave={onMouseLeave}
             >
                 {stringTime}
             </div>
 
             {show && (
-                <div>
+                <div className={s.showDate}>
                     {stringDate}
                 </div>
             )}
-
-            <SuperButton onClick={start}>start</SuperButton>
-            <SuperButton onClick={stop}>stop</SuperButton>
+            <div>
+                <SuperButton green onClick={start}>start</SuperButton>
+                <SuperButton green onClick={stop}>stop</SuperButton>
+            </div>
 
         </div>
     );
