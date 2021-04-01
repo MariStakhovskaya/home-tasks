4a89733949f205b6d9ba23c415dd7d0c2a28ed93
MariStakhovskaya <maryia.jegorova@gmail.com>
2021-04-01 13:50:29 +0300

    Updates

---
+++ precache-manifest.11a49a987502b54adf0d613341dd4f3e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d530f502adb95cf0a07aa3d02781572f",
+    "url": "/home-tasks/index.html"
+  },
+  {
+    "revision": "e7a8546d50cfacda11b5",
+    "url": "/home-tasks/static/css/main.a3966241.chunk.css"
+  },
+  {
+    "revision": "e8b757f7dc6e07283732",
+    "url": "/home-tasks/static/js/2.31a63a1a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/home-tasks/static/js/2.31a63a1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7a8546d50cfacda11b5",
+    "url": "/home-tasks/static/js/main.e6dc0179.chunk.js"
+  },
+  {
+    "revision": "08e17a2123cdaee402e1",
+    "url": "/home-tasks/static/js/runtime-main.e2248177.js"
+  },
+  {
+    "revision": "1340f194fdb7869f41d33d69911549e6",
+    "url": "/home-tasks/static/media/404.1340f194.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/home-tasks/precache-manifest.ef8ab8b231e476b3d5001b2c06680c87.js"
+  "/home-tasks/precache-manifest.11a49a987502b54adf0d613341dd4f3e.js"
 );
 
 self.addEventListener('message', (event) => {
